00bce2f55b9768da71e3acf85ee63a96f8a4b1e7
Adej0la <dejoladaniel@gmail.com>
2022-06-22 11:32:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function arithmetic() {
+  let firstNumber = Number(prompt("input a number"));
+  let operator = prompt("choose an operator e.g *, +, -, or /");
+  let secondNumber = Number(prompt("input a second number"));
+  let result;
+
+  switch (operator) {
+    case "*":
+      result = firstNumber * secondNumber;
+      alert(result);
+      break;
+    case "+":
+      result = firstNumber + secondNumber;
+      alert(result);
+      break;
+    case "-":
+      result = firstNumber - secondNumber;
+      alert(result);
+      break;
+    case "/":
+      result = firstNumber / secondNumber;
+      alert(result);
+      break;
+    default:
+      alert("input is invalid, try again");
+  }
+  location.reload(true);
+}
+
+arithmetic();
